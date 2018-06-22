49b346487fbb9d8f6298ac7e758178434273ea44
sockeqwe <hannes.dorfmann@gmail.com>
2018-06-22 18:35:57 +0200

    Fix Barcamp

--- app/src/main/java/de/droidcon/berlin2018/ui/barcamp/BarCampPresenter.kt
+++ app/src/main/java/de/droidcon/berlin2018/ui/barcamp/BarCampPresenter.kt
@@ -27,103 +27,110 @@ class BarCampPresenter(
     private val okHttp: OkHttpClient,
     private val clock: Clock,
     private val bypass: Bypass,
-    private val sessionsInteractor: SessionsInteractor) : MviBasePresenter<BarCampView, LceViewState<Sessions>>() {
+    private val sessionsInteractor: SessionsInteractor
+) : MviBasePresenter<BarCampView, LceViewState<Sessions>>() {
 
-  private val url = "https://raw.githubusercontent.com/droidcon/gitbook-2017-berlin-barcamp/master/schedule.md"
-
-
-  override fun bindIntents() {
-    val scrolledToNowIntent = intent(BarCampView::scrolledToNowIntent)
-        .doOnNext { Timber.d("Scrolled to now intent: $it") }
-        .distinctUntilChanged()
-
-    val loadDataIntent = intent(BarCampView::loadDataIntent)
-        .doOnNext { Timber.d("Load data intent") }
-
-    val pullToRefreshIntent = intent(BarCampView::swipeRefreshIntent)
-        .doOnNext { Timber.d("PullToRefresh data intent") }
-
-    val retryLoadDataIntent = intent(BarCampView::retryLoadDataIntent)
-        .doOnNext { Timber.d("Retry load data intent") }
-
-    val data = Observable.merge(loadDataIntent, retryLoadDataIntent, pullToRefreshIntent).switchMap {
-
-      val scheduleObservable = Observable.fromCallable {
-        val response = okHttp.newCall(Request.Builder().url(url).get()
-            .cacheControl(CacheControl.FORCE_NETWORK).build()).execute()
-        if (!response.isSuccessful) {
-          throw IOException("Could not load BarCamp")
-        }
-
-        val markdown = response.body()!!.string()
-        markdownToSessions(markdown)
-      }.subscribeOn(Schedulers.io())
-
-      sessionsInteractor.transform(scrolledToNowIntent) {
-        scheduleObservable
-      }
-    }.observeOn(AndroidSchedulers.mainThread())
-
-    subscribeViewState(data, BarCampView::render)
-  }
+    private val url =
+        "https://raw.githubusercontent.com/droidcon/gitbook-2018-berlin-barcamp/master/schedule.md"
 
 
-  /**
-   * Parses the markdown file to get the list of sessions
-   */
-  private fun markdownToSessions(markdown: String): List<Session> {
-    val lines = markdown.split("\n")
-    val sessionLines = lines.filter {
-      val timed = it.trim()
-      timed.isNotEmpty() && timed[0].isDigit()
+    override fun bindIntents() {
+        val scrolledToNowIntent = intent(BarCampView::scrolledToNowIntent)
+            .doOnNext { Timber.d("Scrolled to now intent: $it") }
+            .distinctUntilChanged()
+
+        val loadDataIntent = intent(BarCampView::loadDataIntent)
+            .doOnNext { Timber.d("Load data intent") }
+
+        val pullToRefreshIntent = intent(BarCampView::swipeRefreshIntent)
+            .doOnNext { Timber.d("PullToRefresh data intent") }
+
+        val retryLoadDataIntent = intent(BarCampView::retryLoadDataIntent)
+            .doOnNext { Timber.d("Retry load data intent") }
+
+        val data =
+            Observable.merge(loadDataIntent, retryLoadDataIntent, pullToRefreshIntent).switchMap {
+
+                val scheduleObservable = Observable.fromCallable {
+                    val response = okHttp.newCall(
+                        Request.Builder().url(url).get()
+                            .cacheControl(CacheControl.FORCE_NETWORK).build()
+                    ).execute()
+                    if (!response.isSuccessful) {
+                        throw IOException("Could not load BarCamp")
+                    }
+
+                    val markdown = response.body()!!.string()
+                    markdownToSessions(markdown)
+                }.subscribeOn(Schedulers.io())
+
+                sessionsInteractor.transform(scrolledToNowIntent) {
+                    scheduleObservable
+                }
+            }.observeOn(AndroidSchedulers.mainThread())
+
+        subscribeViewState(data, BarCampView::render)
     }
-    var id = 0
 
-    return sessionLines.flatMap { line ->
 
-      val slots = line.split("|")
-      if (slots.isEmpty())
-        emptyList<Session>()
-      else {
-        val hourMinuteStr = slots[0].trim().split(":")
-        val hour = hourMinuteStr[0].trim().toInt()
-        val minute = hourMinuteStr[1].trim().toInt()
-        val startTime = ZonedDateTime.of(2017, 9, 3, hour, minute, 0, 0,
-            clock.getZoneConferenceTakesPlace())
-        val endTime = startTime.plusMinutes(20)
+    /**
+     * Parses the markdown file to get the list of sessions
+     */
+    private fun markdownToSessions(markdown: String): List<Session> {
+        val lines = markdown.split("\n")
+        val sessionLines = lines.filter {
+            val timed = it.trim()
+            timed.isNotEmpty() && timed[0] == '|' && timed.length > 3 && timed[2].isDigit()
+        }
+        var id = 0
 
-        val sessionsInLine = ArrayList<Session>()
+        return sessionLines.flatMap { line ->
 
-        slots.subList(1, slots.size).forEachIndexed { i, title ->
-          val trimmed = title.trim()
-          if (trimmed.isNotEmpty() && trimmed != "BREAK" && trimmed != "-") {
-            sessionsInLine.add(
-                SessionAutoValue.create(
-                    id = "Barcamp" + id++,
-                    speakers = emptyList(),
-                    favorite = false,
-                    title = bypass.markdownToSpannable(title).toString(),
-                    description = "",
-                    locationName = "Stage ${getStage(i)}",
-                    locationId = "Stage ${getStage(i)}",
-                    start = startTime.toInstant(),
-                    end = endTime.toInstant(),
-                    tags = null
+            val slots = line.split("|").filter { it.isNotBlank() }
+            if (slots.isEmpty())
+                emptyList<Session>()
+            else {
+                val hourMinuteStr = slots[0].trim().split(":")
+                val hour = hourMinuteStr[0].trim().toInt()
+                val minute = hourMinuteStr[1].trim().toInt()
+                val startTime = ZonedDateTime.of(
+                    2018, 6, 25, hour, minute, 0, 0,
+                    clock.getZoneConferenceTakesPlace()
                 )
-            )
-          }
+                val endTime = startTime.plusMinutes(30)
+
+                val sessionsInLine = ArrayList<Session>()
+
+                slots.subList(1, slots.size).forEachIndexed { i, title ->
+                    val trimmed = title.trim()
+                    if (trimmed.isNotEmpty() && trimmed != "BREAK" && trimmed != "-") {
+                        sessionsInLine.add(
+                            SessionAutoValue.create(
+                                id = "Barcamp" + id++,
+                                speakers = emptyList(),
+                                favorite = false,
+                                title = bypass.markdownToSpannable(title).toString(),
+                                description = "",
+                                locationName = getStage(i),
+                                locationId = getStage(i),
+                                start = startTime.toInstant(),
+                                end = endTime.toInstant(),
+                                tags = null
+                            )
+                        )
+                    }
+                }
+
+                sessionsInLine
+            }
         }
-
-        sessionsInLine
-      }
     }
-  }
 
-  private fun getStage(index : Int) = when (index){
-    0 -> "Stage Lamarr"
-    1 -> "Stage Lovelace"
-    2 -> "Stage Turing"
-    3 -> "Stage Zuse"
-    else -> "Stage Unknown"
-  }
+    private fun getStage(index: Int) = when (index) {
+        0 -> "Oreo"
+        1 -> "Cupcake"
+        2 -> "Lollipop"
+        3 -> "Jelly Bean"
+        else -> "Stage Unknown"
+    }
 }
